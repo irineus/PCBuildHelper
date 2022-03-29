65daf4beb0b835b1cf8d8fd3cf23fe9e43206d7c
Irineu Junior Pinheiro dos Santos <irineus@gmail.com>
2022-03-29 09:24:34 -0300

    Data Annotations finalizado

--- PCBuildWeb/Models/Entities/Parts/Storage.cs
+++ PCBuildWeb/Models/Entities/Parts/Storage.cs
@@ -1,14 +1,26 @@
 ﻿using PCBuildWeb.Models.Entities.Bases;
 using PCBuildWeb.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace PCBuildWeb.Models.Entities.Parts
 {
     public class Storage : ComputerPart
     {
+        [Display(Name = "Type")]
+        [Required(ErrorMessage = "{0} is required")]
+        [EnumDataType(typeof(StorageType))]
         public StorageType Type { get; set; }
+        [Display(Name = "Size (GB)")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100, 6000, ErrorMessage = "{0} should be a value between {1} and {2}")]
         public int Size { get; set; }
+        [Display(Name = "Speed (MB/s)")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100, 10000, ErrorMessage = "{0} should be a value between {1} and {2}")]
         public int Speed { get; set; }
+        [Display(Name = "Includes Heatsink?")]
         public bool IncludesHeatsink { get; set; }
+        [Display(Name = "Heatsink Thickness")]
         public double HeatsinkThickness { get; set; }
 
     }
